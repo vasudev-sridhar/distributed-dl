e05cd1b86187908b637cee503bcd40172c5add14
vasudev-sridhar <vasudev.sridhar007@gmail.com>
2021-04-06 17:09:55 -0700

    Added distribution 1

--- another_neural_net.py
+++ another_neural_net.py
@@ -1,26 +1,41 @@
+import argparse
 import torch
 from torch import nn
 from torch import optim
 import torch.nn.functional as F
 from torchvision import datasets, transforms
+from torch.utils.data.distributed import DistributedSampler
+from torch.utils.data import DataLoader
 
+# prase the local_rank argument from command line for the current process
+parser = argparse.ArgumentParser()
+parser.add_argument("--local_rank", default=0, type=int)
+args = parser.parse_args()
+
+# setup the distributed backend for managing the distributed training
+torch.distributed.init_process_group('nccl')
 # Define a transform to normalize the data
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.5,), (0.5,)),
                               ])
 # Download and load the training data
 trainset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+# trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+# Setup the distributed sampler to split the dataset to each GPU.
+dist_sampler = DistributedSampler(trainset)
+trainloader = DataLoader(trainset, sampler=dist_sampler)
+
 
 if torch.cuda.is_available():
     print("Cuda Device Available")
     device_ids = list(range(torch.cuda.device_count()))
     print(device_ids)
-    device = torch.device("cuda:0")
+    device = torch.device("cuda", args.local_rank)
     print("Name of the Cuda Device: ", torch.cuda.get_device_name())
     print("GPU Computational Capablity: ", torch.cuda.get_device_capability())
 else:
-    device = torch.device("cpu")
+    device = torch.device("cpu", args.local_rank)
     print('No GPU. switching to CPU')
 
 model = nn.Sequential(nn.Linear(784, 128),
